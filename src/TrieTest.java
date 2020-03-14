ef20e5036a8c1be3660df5b4d69604c372e91153
LucadeRaad <59776518+LucadeRaad@users.noreply.github.com>
2020-03-13 20:46:43 -0700

    all tests done, all functions working. Success

--- src/TrieTest.java
+++ src/TrieTest.java
@@ -1,12 +1,103 @@
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class TrieTest {
     // I want to test creating trie trees
     @Test
     public void testCreate() {
+        TrieTree tree = testCases();
+        List<String> print = tree.printTrie();
+        System.out.println(print.toString());
+    }
+
+    @Test
+    public void testAutocompleteOneLetter() {
+        TrieTree tree = testCases();
+        String[] expectedData = {
+                "hello",
+                "high",
+                "hollow",
+                "how"
+        };
+        List<String> expected = new ArrayList<>();
+        Collections.addAll(expected, expectedData); // IntelliJ wanted me to do this
+
+        List<String> actual = tree.find("h");
+
+        for(int i = 0; i < actual.size(); i++)
+            assert(actual.equals(expected));
+    }
+
+    @Test
+    public void testAutocompleteTwoLetters() {
+        TrieTree tree = testCases();
+        String[] expectedData = {
+                "hollow",
+                "how"
+        };
+        List<String> expected = new ArrayList<>();
+        Collections.addAll(expected, expectedData); // IntelliJ wanted me to do this
+
+        List<String> actual = tree.find("ho");
+
+        for(int i = 0; i < expected.size(); i++) {
+            assert (actual.get(i).equals(expected.get(i)));
+        }
+    }
+
+    @Test
+    public void testAutocompleteThreeLetters() {
+        TrieTree tree = testCases();
+        String[] expectedData = {
+                "seatac",
+                "seattle"
+        };
+        List<String> expected = new ArrayList<>();
+        Collections.addAll(expected, expectedData); // IntelliJ wanted me to do this
+
+        List<String> actual = tree.find("sea");
+
+        for(int i = 0; i < expected.size(); i++) {
+            assert (actual.get(i).equals(expected.get(i)));
+        }
+    }
+
+    @Test
+    public void testAutocompleteFourLetters() {
+        TrieTree tree = testCases();
+        String[] expectedData = {
+                "hollow",
+        };
+        List<String> expected = new ArrayList<>();
+        Collections.addAll(expected, expectedData); // IntelliJ wanted me to do this
+
+        List<String> actual = tree.find("holl");
+
+        for(int i = 0; i < expected.size(); i++) {
+            assert (actual.get(i).equals(expected.get(i)));
+        }
+    }
+
+    @Test
+    public void testAutocompleteNonExistent() {
+        TrieTree tree = testCases();
+        String[] expectedData = {
+
+        };
+        List<String> expected = new ArrayList<>();
+        Collections.addAll(expected, expectedData); // IntelliJ wanted me to do this
+
+        List<String> actual = tree.find("luca");
+
+        for(int i = 0; i < expected.size(); i++) {
+            assert (actual.get(i).equals(expected.get(i)));
+        }
+    }
+
+    public TrieTree testCases() {
         TrieTree tree = new TrieTree();
         List<String> test = new ArrayList<>();
         test.add("hello");
@@ -17,7 +108,6 @@ public class TrieTest {
         test.add("hollow");
         test.add("how");
         tree.createTree(test);
-        List<String> print = tree.printTrie();
-        System.out.println(print.toString());
+        return tree;
     }
 }
--- src/TrieTree.java
+++ src/TrieTree.java
@@ -3,6 +3,7 @@ import java.util.List;
 
 public class TrieTree {
     TrieNode root = new TrieNode();
+
     // This creates a tree. A whole list will be put in and the strings inside will be the paths
     public void createTree(List<String> input) {
         // Separating list strings. We now have individual words.
@@ -24,24 +25,51 @@ public class TrieTree {
         }
     }
 
+    public List<String> find(String word) {
+        // We want to store everything put into output
+        List<String> output = new ArrayList<>();
+        // Once done with a branch of the trie, we no longer need to store that info
+        char[] chars = new char[400];
+        findHelper(word, root, chars, 0, output, 0);
+        return output;
+    }
+
+    private void findHelper(String word, TrieNode current, char[] charStack, int stackDepth, List<String> output, int wordDepth) {
+        if (current.isEndOfWord)
+            output.add(new String(charStack, 0, stackDepth));
+
+        for (int i = 0; i < current.val.length; i++) {
+            if (current.val[i] != null) {
+                if (wordDepth < word.length() - 1 && current.val[i].letter == word.charAt(wordDepth)) {
+                    charStack[stackDepth] = current.val[i].letter;
+                    findHelper(word, current.val[i], charStack, stackDepth + 1, output, wordDepth + 1);
+                }
+                else if (current.val[i].letter == word.charAt(wordDepth)) {
+                    charStack[stackDepth] = current.val[i].letter;
+                    printHelper(current.val[i], charStack, stackDepth + 1, output);
+                }
+            }
+        }
+    }
+
     // Gives a list String of my trie used primarily for debugging
     public List<String> printTrie() {
         List<String> output = new ArrayList<>();
         char[] chars = new char[400];
-        printHelper(root, chars, 0,  output);
+        printHelper(root, chars, 0, output);
         return output;
     }
 
     // Uses recursion to go through all my nodes
-    private void printHelper(TrieNode trieNode, char[] charStack, int stackDepth, List<String> result) {
+    private void printHelper(TrieNode trieNode, char[] charStack, int stackDepth, List<String> output) {
         if (trieNode.isEndOfWord)
-            result.add(new String(charStack, 0, stackDepth));
+            output.add(new String(charStack, 0, stackDepth));
 
         // Inefficient. It has to go through every input in the array until it gets a filled input
-        for(int i = 0; i < trieNode.val.length; i++) {
-            if(trieNode.val[i] != null) {
+        for (int i = 0; i < trieNode.val.length; i++) {
+            if (trieNode.val[i] != null) {
                 charStack[stackDepth] = trieNode.val[i].letter;
-                printHelper(trieNode.val[i], charStack, stackDepth + 1, result);
+                printHelper(trieNode.val[i], charStack, stackDepth + 1, output);
             }
         }
     }
